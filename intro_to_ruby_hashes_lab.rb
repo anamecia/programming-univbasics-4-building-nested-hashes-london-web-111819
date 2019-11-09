9a0dbabd8c6a1f29e85a1394f979c2b16480ac40
Ana Robalo <anameciarobalo@gmail.com>
2019-11-09 20:40:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,5 +42,6 @@ def monopoly_with_fourth_tier
   # complexity.
   new_new_new_hash = monopoly_with_third_tier
   new_new_new_hash[:railroads][:names][:reading_railroad] = {"mortgage_value" => "$100"}
+  new_new_new_hash[:railroads][:names][:pennsylvania_railroad] = {"mortgage_value" => "$200"}
   new_new_new_hash
 end
